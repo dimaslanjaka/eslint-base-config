8cf30c005bb82b442a845e260f6982dcda7ef4b1
dimaslanjaka <dimaslanjaka@gmail.com>
2026-05-29 04:15:08 +0700

    fix(vscode): enhance PowerShell Oh My Posh initialization to fallback on default if custom scripts are not found

--- src/_auto_gen/checksum.cjs
+++ src/_auto_gen/checksum.cjs
@@ -1,4 +1,4 @@
-const hash = 'e5405a5db2b11d8d9f12dc6e87ef61a1cd3bf7c02a77f78ab595b98c7d98ec2a';
+const hash = '6c2a1168139eae9f1714a577af6f5e0f6687b8b6d1af4dac879c7c1aa96080ed';
 
 module.exports = hash;
 module.exports = { hash };
