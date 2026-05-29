7e13f11b9416555bd02ae0ebe11b5357a5c321e4
Dimas Lanjaka <dimaslanjaka@gmail.com>
2026-05-29 10:39:00 +0000

    fix(OhMyPoshInit): add global npm paths as fallback for oh-my-posh config resolution

--- src/_auto_gen/checksum.cjs
+++ src/_auto_gen/checksum.cjs
@@ -1,4 +1,4 @@
-const hash = '6280ad786265cd819070d9eb99ea0fbc81da27c137c7badcde271880b59b72f2';
+const hash = 'b641e5fccd681b79dda0c51ee2ea158c020dec0fb57a72759eafbe6945a07fe2';
 
 module.exports = hash;
 module.exports = { hash };
