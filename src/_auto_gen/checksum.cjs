77c8bce71609a5456e07a8ef5c41879cc9ed2452
dimaslanjaka <dimaslanjaka@gmail.com>
2026-05-30 13:33:00 +0700

    fix(terminal): simplify Oh My Posh initialization command in PowerShell profile

--- src/_auto_gen/checksum.cjs
+++ src/_auto_gen/checksum.cjs
@@ -1,4 +1,4 @@
-const hash = 'aacf3161fe2f4d9021d2e57e906fe7969d81a7c0c812ba77990279d4667f33f9';
+const hash = '9e67b79115b38d31b0574dd81d533b6aecfce2add96b2ce8afc03081eca927e1';
 
 module.exports = hash;
 module.exports = { hash };
